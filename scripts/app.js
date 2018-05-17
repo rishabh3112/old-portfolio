1a7d7e80ea9f18cba60c63e1edbd135ff6a4b97a
Rishabh <rishabh31121999@gmail.com>
2018-05-17 08:59:52 +0530

    new: add scroll reveal and image list

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,13 @@ import {MDCTopAppBar} from '@material/top-app-bar/index';
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+window.sr = ScrollReveal();
+sr.reveal(".mdc-card",{
+    reset: true,
+    scale: 0.8,
+    origin: "bottom",
+    easing:"cubic-bezier(0.0, 0.0, 0.2, 1)",
+    container: document.querySelector("main"),
+    viewFactor: 0.5,
+},200);
